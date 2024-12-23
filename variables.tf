356caaecb12a995b31d620749855ed1d0923344f
nitinn94 <nitin.naveen100@gmail.com>
2024-12-23 10:33:38 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,40 +1,19 @@
-variable "region" {
-  description = "The AWS region to create resources in"
+# variables.tf
+
+variable "instance_type" {
+  description = "The type of instance to create"
   type        = string
-  default     = "us-east-1"  # Default AWS region
+  default     = "t2.micro"  # Default value (if not provided)
 }
 
 variable "ami_id" {
-  description = "The AMI ID for the EC2 instance"
+  description = "The AMI ID to use for the instance"
   type        = string
-  default     = "ami-0c55b159cbfafe1f0"  # Amazon Linux 2 AMI (US-West-2), update as necessary
-}
-
-variable "instance_type" {
-  description = "The EC2 instance type"
-  type        = string
-  default     = "t2.micro"  # You can change this to any EC2 type, e.g., `t2.small`
+  default     = "ami-0c55b159cbfafe1f0"  # Amazon Linux 2 AMI ID (us-west-2 region)
 }
 
 variable "key_name" {
-  description = "The SSH key pair to access the EC2 instance"
+  description = "The SSH key name to access the EC2 instance"
   type        = string
-}
-
-variable "docker_image" {
-  description = "The Docker image to run on the EC2 instance"
-  type        = string
-  default     = "nginx:latest"  # Replace with your custom Docker image if needed
-}
-
-variable "docker_port" {
-  description = "The port on which the Docker container will listen"
-  type        = number
-  default     = 80
-}
-
-variable "docker_container_name" {
-  description = "The name of the Docker container"
-  type        = string
-  default     = "my-app-container"
+  default     = "my-key-pair"  # Replace with your actual key name
 }
